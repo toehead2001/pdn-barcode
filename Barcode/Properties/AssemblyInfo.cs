745bb66ba76bf96e5a1aac46d471c7a655bdc72e
toehead2001 <toe_head2001@yahoo.com>
2015-08-24 17:49:06 -0600

    Assembly file update
    version bump, new keyword

--- Barcode/Properties/AssemblyInfo.cs
+++ Barcode/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Barcode Effect")]
-[assembly: AssemblyDescription("Code 39 Barcode Generator")]
-[assembly: AssemblyConfiguration("Barcode|Code 39|POSTNET")]
+[assembly: AssemblyDescription("Barcode Generator")]
+[assembly: AssemblyConfiguration("Barcode|Code 39|POSTNET|UPC")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Barcode Effect")]
 [assembly: AssemblyCopyright("Copyright © Michael J. Sepcot & toe_head2001")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
