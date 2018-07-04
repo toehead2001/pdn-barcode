07e1e615d1cdd9d0d3feca2610bdc33740866b58
toe_head2001 <toe_head2001@yahoo.com>
2018-07-03 18:16:28 -0600

    Use generics form of GetCustomAttribute

--- Barcode/Barcode.cs
+++ Barcode/Barcode.cs
@@ -12,15 +12,14 @@ namespace Barcode
 {
     public class PluginSupportInfo : IPluginSupportInfo
     {
-        public string Author => ((AssemblyCopyrightAttribute)base.GetType().Assembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0]).Copyright;
-        public string Copyright => ((AssemblyDescriptionAttribute)base.GetType().Assembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)[0]).Description;
-        public string DisplayName => ((AssemblyProductAttribute)base.GetType().Assembly.GetCustomAttributes(typeof(AssemblyProductAttribute), false)[0]).Product;
+        public string Author => base.GetType().Assembly.GetCustomAttribute<AssemblyCopyrightAttribute>().Copyright;
+        public string Copyright => base.GetType().Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+        public string DisplayName => base.GetType().Assembly.GetCustomAttribute<AssemblyProductAttribute>().Product;
         public Version Version => base.GetType().Assembly.GetName().Version;
         public Uri WebsiteUri => new Uri("https://forums.getpaint.net/index.php?showtopic=31559");
     }
 
     [PluginSupportInfo(typeof(PluginSupportInfo), DisplayName = "Barcode")]
-
     public class Barcode : Effect<BarcodeConfigToken>
     {
         private const string StaticName = "Barcode";
